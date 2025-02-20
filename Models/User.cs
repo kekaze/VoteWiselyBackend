81b5b0e4a8e55ad7d2ef03331141a503754723c1
kekaze <jroyceyanson@gmail.com>
2025-02-20 21:30:39 +0800

    setup user model

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace VoteWiselyBackend.Models
+{
+    [Table("users")]
+    public class User : BaseModel
+    {
+        [PrimaryKey("id")]
+        public required string Id { get; set; }
+        [Column("name")]
+        public required string FullName { get; set; }
+        [Column("email")]
+        public required string Email { get; set; }
+        [Column("create_at")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
