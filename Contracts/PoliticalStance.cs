673f4b85366ea303f2e82b423de3a01b2885d3ff
kekaze <jroyceyanson@gmail.com>
2025-02-22 13:30:42 +0800

    WIP: authentication process, create endpoint for similarity-search

---
+++ Contracts/PoliticalStance.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VoteWiselyBackend.Contracts
+{
+    public class PoliticalStance
+    {
+        [JsonPropertyName("in_favor")]
+        public List<string> InFavor { get; set; } = new List<string>();
+
+        public List<string> Against { get; set; } = new List<string>();
+
+        public List<string> Platforms { get; set; } = new List<string>();
+    }
+}
