ac2ff285d3157e3f692309d41e5e3a2af84c9677
kekaze <jroyceyanson@gmail.com>
2025-02-22 13:39:46 +0800

    prepare text for vectorization

--- Controllers/EmbeddingSearchController.cs
+++ Controllers/EmbeddingSearchController.cs
@@ -16,8 +16,17 @@ namespace VoteWiselyBackend.Controllers
         [HttpPost("similarity-search")]
         public async Task<IActionResult> PerformSimilaritySearch([FromBody] PoliticalStance candidateCriteria)
         {
-            Console.Write(candidateCriteria);
-            return Ok(candidateCriteria);
+            // Process candidateCriteria into a text in preparation for vectorization
+            string inFavor = string.Join(", ", candidateCriteria.InFavor);
+            string against = string.Join(", ", candidateCriteria.Against);
+            string platforms = string.Join(", ", candidateCriteria.Platforms);
+            string criteria = $"In favor to: {inFavor}\nAgainst: {against}\nPlatforms: {platforms}";
+            // Vectorize the text
+            // Perform similarity search
+            // Receive top 12 with most similarity
+            // Prepare received data
+            // Send clean data to front end
+            return Ok(criteria);
         }
     }
 }
