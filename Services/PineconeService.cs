aab55b166309c01189529fa75273e853af247872
kekaze <jroyceyanson@gmail.com>
2025-03-04 22:05:33 +0800

    WIP: service for querying against a Pinecone index

--- Services/PineconeService.cs
+++ Services/PineconeService.cs
@@ -12,5 +12,27 @@ namespace VoteWiselyBackend.Services
             _pineconeClient = pineconeClient;
             _indexName = indexName;
         }
+
+        public async Task QueryIndexAsync(float[] queryVector, uint topK = 5)
+        {
+            var index = _pineconeClient.Index(_indexName);
+
+            var queryRequest = new QueryRequest
+            {
+                TopK = topK,
+                Vector = queryVector,
+                IncludeMetadata = true
+            };
+
+            var response = await index.QueryAsync(
+                new QueryRequest
+                {
+                    TopK = topK,
+                    Vector = queryVector,
+                    IncludeMetadata = true
+                }
+            );
+        }
+
     }
 }
