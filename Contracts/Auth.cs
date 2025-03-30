0b1e8813a5b9410215c30edad7788350fdb05f08
kekaze <jroyceyanson@gmail.com>
2025-03-30 17:50:45 +0800

    implement signup endpoint with user metadata

---
+++ Contracts/Auth.cs
@@ -0,0 +1,10 @@
+﻿namespace VoteWiselyBackend.Contracts
+{
+    public class SignUpRequest
+    {
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string ConfirmPassword { get; set; }
+    }
+}
