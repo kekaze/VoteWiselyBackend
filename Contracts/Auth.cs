2fa951910507ba86ba2a631a5c2f9988980f1795
kekaze <jroyceyanson@gmail.com>
2025-04-05 21:23:39 +0800

    map signup request

--- Contracts/Auth.cs
+++ Contracts/Auth.cs
@@ -1,10 +1,14 @@
-﻿namespace VoteWiselyBackend.Contracts
+﻿using System.Text.Json.Serialization;
+
+namespace VoteWiselyBackend.Contracts
 {
     public class SignUpRequest
     {
+        [JsonPropertyName("full_name")]
         public required string FullName { get; set; }
         public required string Email { get; set; }
         public required string Password { get; set; }
+        [JsonPropertyName("confirm_password")]
         public required string ConfirmPassword { get; set; }
     }
 
